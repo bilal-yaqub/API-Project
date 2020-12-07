e96ec99bda0663789342faf0a153de3a3a625329
Bilal Yaqub <bilal._.yaqub@hotmail.com>
2020-12-07 13:58:29 +0000

    Takes user input and displays relevant jokes

--- ApiProject.py
+++ ApiProject.py
@@ -2,16 +2,22 @@ import requests
 
 url = ("https://icanhazdadjoke.com/search")   # Adding search at the end so I can add parameters
 
-# Requests for the file in JavaScript Object Notation
-response = requests.get(url, headers={"Accept": "application/json"})
+userInput = input("What kind of joke do you want? ")
+
+# Searches for whatever user inputted
+response = requests.get(url, headers={"Accept": "application/json"}, params={"term": (userInput)})
 
 data = response.json()      # Returns a dictionary with all the data; all the jokes have the key "result"
 
 joke = data["results"]      # ["results] returns all jokes as a list
 
-joke = joke[0]  # This returns the first joke as a dictionary along with its key
+# If there is no joke returns Try again! Otherwise outputs the joke
+if joke == []:
+    print("Try again!")
+else:
+    joke = joke[0]  # This returns the first joke as a dictionary along with its key
 
-joke = list(joke.values())  # This returns a list with the joke at index [1]
+    joke = list(joke.values())  # This returns a list with the joke at index [1]
 
-joke = joke[1]
-print(joke)
+    joke = joke[1]
+    print(joke)
